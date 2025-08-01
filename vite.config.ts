e708ce9e84e067817d4388470b78cd8b3beba28d
fuadnafiz98 <fuadnafiz98@gmail.com>
2025-08-01 22:55:49 +0600

    fix: vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from "vite-plugin-vercel";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()],
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
